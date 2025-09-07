4b3845e8a36b9b4848a9cb0ff2f27ab9c0f5fb79
Shabab391 <136697622+Shabab391@users.noreply.github.com>
2025-09-08 05:19:34 +0600

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"
 dotenv.config();
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -14,10 +15,15 @@ import profileRoutes from "./routes/profileRoutes.js";
 
 const app= express();
 const PORT=process.env.PORT ||5001;
+const __dirname=path.resolve()
 
 //middleware
+if (process.env.MODE_ENV !=="production"){
+    app.use(cors());}
+
+
 app.use(express.json());
-app.use(cors());
+
 
 app.use("/api/notes", notesRoutes);
 app.use("/api/search", searchRoutes);
@@ -25,6 +31,15 @@ app.use("/api/place", placeRoutes);
 app.use("/api/location", locationRoutes);
 app.use("/api/login", loginRoutes);
 app.use("/api/profile", profileRoutes);
+
+
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(_dirname,"../frontend","dist","index.html"))
+})
+}
+
 
 connectDB().then(()=>{
 
